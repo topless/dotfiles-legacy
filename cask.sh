6aede66ab1b4a1b0f7dc51e320486afda29ca59d
Panayiotis Lipiridis <lipiridis@gmail.com>
2016-10-01 10:59:23 +0200

    Readme and cask

--- cask.sh
+++ cask.sh
@@ -2,20 +2,20 @@
 
 # Install native apps
 
-brew tap caskroom/cask
+brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 brew cask install atom 2> /dev/null
 brew cask install dropbox 2> /dev/null
-brew cask install firefox 2> /dev/null
+brew cask install firefox-beta 2> /dev/null
 brew cask install google-chrome-canary 2> /dev/null
 brew cask install google-chrome-dev 2> /dev/null
+brew cask install google-earth 2> /dev/null
 brew cask install imagealpha 2> /dev/null
 brew cask install imageoptim 2> /dev/null
 brew cask install iterm2 2> /dev/null
 brew cask install skype 2> /dev/null
 brew cask install slack 2> /dev/null
-brew cask install the-unarchiver 2> /dev/null
 brew cask install utorrent 2> /dev/null
 brew cask install vlc 2> /dev/null
 brew cask install xquartz 2> /dev/null
