23dec904982c7acbdbfeec9f8abfb4d80fde652b
Panayiotis Lipiridis <lipiridis@gmail.com>
2016-10-04 11:29:21 +0200

    Npm pips

--- npm.sh
+++ npm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+npm install -g git-open
+npm install -g grunt
 npm install -g gulp
+npm install -g npm-check-updates
 npm install -g trash-cli
 npm install -g ungit
--- pip.sh
+++ pip.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 pip install babel
+pip install flake8
 pip install flask
 pip install requests
 pip install virtualenv
